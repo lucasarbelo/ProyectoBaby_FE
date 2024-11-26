66f343dc54bc9c3b5094a21c64cd6915182e1cba
Lucas Arbelo <lucasarbelocontact@gmail.com>
2024-11-26 12:55:00 -0300

    agregue el yml

--- Code/vite.config.js
+++ Code/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+base: "/lucasarbelo/Proyecto_FRONTEND/"
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
