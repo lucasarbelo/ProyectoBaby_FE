2c835f4068ffd811ab50d27cbc06d358b93bef37
Lucas Arbelo <lucasarbelocontact@gmail.com>
2025-07-14 01:10:08 -0300

    pages

--- Code/vite.config.js
+++ Code/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-base: "/lucasarbelo/Proyecto_FRONTEND/"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/ProyectoBaby_FE/"
 })
